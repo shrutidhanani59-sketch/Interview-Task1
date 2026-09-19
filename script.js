07bfc51dc4c9e01a4a402dde9af7632b2e36ed6c
Shruti Dhanani <tamaro-github-email@example.com>
2026-09-19 12:53:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+document.querySelector('.homeScreen p').style.display = "blobk";
+document.querySelector('.gameScreen').style.display = "none";
+
+document.querySelector('.homeScreen p').onclick = function () {
+
+    document.querySelector('.gameScreen').style.display = "block";
+    document.querySelector('.homeScreen p').style.display = "none";
+}
+
+
+
+
+
+document.querySelector('.userNumber').onchange = function () {
+    document.querySelector('.box').innerHTML = document.querySelector('.userNumber').value;
+
+
+    console.log(Math.floor(Math.random));
+
+// if(Math.random > document.querySelector('.userNumber').value)
+// {
+//     alert(" guess Number is high");
+// }
+// else{
+//     alert(" guess Number is low");
+// }
+
+}
+
+
